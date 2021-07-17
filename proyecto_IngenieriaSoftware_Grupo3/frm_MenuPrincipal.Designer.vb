f5c08ee23c456cb947719e1e169da2b9ed8495e6
CARLOS CESAR MEDINA GONZALEZ <carlos.cmedina@hotmail.com>
2021-07-17 15:14:27 -0500

    Creación del proyecto, se cambia el Form1 por frm_MenuPrincipal

--- proyecto_IngenieriaSoftware_Grupo3/frm_MenuPrincipal.Designer.vb
+++ proyecto_IngenieriaSoftware_Grupo3/frm_MenuPrincipal.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class frm_MenuPrincipal
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,10 +22,17 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'frm_MenuPrincipal
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.Name = "frm_MenuPrincipal"
+        Me.Text = "Menú principal"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
