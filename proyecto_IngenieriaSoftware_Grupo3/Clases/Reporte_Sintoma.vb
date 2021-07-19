bf2873cb1560c32c1b725e679cfa542c126f2eaa
JOSE ALFREDO CAVAGNARO ALVARADO <jcavag@hotmail.com>
2021-07-19 11:50:59 -0500

    prueba4

--- proyecto_IngenieriaSoftware_Grupo3/Clases/Reporte_Sintoma.vb
+++ proyecto_IngenieriaSoftware_Grupo3/Clases/Reporte_Sintoma.vb
@@ -1,3 +1,17 @@
 ﻿Public Class Reporte_Sintoma
+    Private idReporte As Integer
+    Private descripcion As String
+    Private idEmpleado As Integer
 
+
+    'Constructor, donde debemos pasar todos los datos  
+    Public Sub New(idReporte As Integer, descripcion As String, idEmpleado As Integer)
+        Me.idReporte = idReporte
+        Me.descripcion = descripcion
+        Me.idEmpleado = idEmpleado
+    End Sub
+
+    Public Sub New()
+
+    End Sub
 End Class
