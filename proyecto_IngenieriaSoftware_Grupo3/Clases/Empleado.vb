ba4b6e13318b67bc7fd602871e8f8a7654b25e10
richard <rintriagov@gmail.com>
2021-07-19 11:35:11 -0500

    CLases Medico y Empleado

--- proyecto_IngenieriaSoftware_Grupo3/Clases/Empleado.vb
+++ proyecto_IngenieriaSoftware_Grupo3/Clases/Empleado.vb
@@ -1,3 +1,18 @@
 ﻿Public Class Empleado
+    Private idEmpleado As Integer
+    Private estado As Boolean
 
+    'Constructor, donde debemos pasar todos los datos  
+    Public Sub New(idEmpleado As Integer, estado As String)
+        Me.idEmpleado = idEmpleado
+        Me.estado = estado
+    End Sub
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub consultaDatos()
+
+    End Sub
 End Class
--- proyecto_IngenieriaSoftware_Grupo3/Clases/Medico.vb
+++ proyecto_IngenieriaSoftware_Grupo3/Clases/Medico.vb
@@ -1,3 +1,22 @@
 ﻿Public Class Medico
+    Private idMedico As Integer
+    Private estado As Boolean
 
+    'Constructor, donde debemos pasar todos los datos  
+    Public Sub New(idMedico As Integer, estado As String)
+        Me.idMedico = idMedico
+        Me.estado = estado
+    End Sub
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub obtenerReporte()
+
+    End Sub
+
+    Public Sub consultaDatos()
+
+    End Sub
 End Class
