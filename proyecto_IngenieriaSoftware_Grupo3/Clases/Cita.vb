05fde11c0200ed932ea93ad123f79c70dd44d265
WIMPER ARLES TIGRERO SORIANO <watigrero@utpl.edu.ec>
2021-07-19 21:21:08 -0500

    Examen y Cita creadas

---
+++ proyecto_IngenieriaSoftware_Grupo3/Clases/Cita.vb
@@ -0,0 +1,28 @@
+﻿Public Class Cita
+    Private num_expediente As Integer
+    Private idCita As Integer
+    Private idEmpleado As Integer
+    Private idExamen As Integer
+    Private idMedico As Integer
+    Private fecha_visita As Date
+    Private horario As horario As String
+
+    Public Sub New(num_expediente As Integer, idCita As Integer, idEmpleado As Integer, idExamen As Integer, idMedico As Integer, fecha_visita As Date, horario As horario As String)
+        Me.num_expediente = num_expediente
+        Me.idCita = idCita
+        Me.idEmpleado = idEmpleado
+        Me.idExamen = idExamen
+        Me.idMedico = idMedico
+        Me.fecha_visita = fecha_visita
+        Me.horario = horario
+
+    End Sub
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub consultaDatos()
+
+    End Sub
+End Class
---
+++ proyecto_IngenieriaSoftware_Grupo3/Clases/Examen.vb
@@ -0,0 +1,22 @@
+﻿Public Class Examen
+    Private idExamen As Integer
+    Private idEmpleado As Integer
+    Private idMedico As Integer
+    Private diagnostico As horario As String
+
+    Public Sub New(idExamen As Integer, idEmpleado As Integer, idMedico As Integer, diagnostico As String)
+        Me.idExamen = idExamen
+        Me.idEmpleado = idEmpleado
+        Me.idMedico = idMedico
+        Me.diagnostico = horario
+
+    End Sub
+
+    Public Sub New()
+
+    End Sub
+
+    Public Sub consultaDatos()
+
+    End Sub
+End Class
