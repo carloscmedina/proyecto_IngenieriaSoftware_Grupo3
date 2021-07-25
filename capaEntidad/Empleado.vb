66a7cbae0406d314910bee33c184826fff5831e7
CARLOS CESAR MEDINA GONZALEZ <carlos.cmedina@hotmail.com>
2021-07-25 15:00:36 -0500

    Habilitada la pantalla Usuarios

--- capaEntidad/Empleado.vb
+++ capaEntidad/Empleado.vb
@@ -10,18 +10,22 @@
 
     Public Property idPersona As Integer
         Get
+            'retorna el id
             Return _idPersona
         End Get
         Set(value As Integer)
+            'viene asignado valor
             _idPersona = value
         End Set
     End Property
 
     Public Property area As String
         Get
+            'retorna area
             Return _area
         End Get
         Set(value As String)
+            'se asigna valor
             _area = value
         End Set
     End Property
